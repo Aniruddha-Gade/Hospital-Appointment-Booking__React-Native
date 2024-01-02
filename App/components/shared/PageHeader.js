78c09fd20dd175ffcc774046ee960fa07c463f85
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-01-02 18:10:17 +0530

    add: show backBtn on condition

--- App/components/shared/PageHeader.js
+++ App/components/shared/PageHeader.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { View, Text, TouchableOpacity} from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
-const PageHeader = ({ title }) => {
+const PageHeader = ({ title, backBtn = true }) => {
 
     const navigation = useNavigation();
 
     return (
         <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-            <TouchableOpacity onPress={() => navigation.goBack()}>
-                <Ionicons name="arrow-back-circle-outline" size={37} color="black" />
-            </TouchableOpacity>
+            {backBtn &&
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Ionicons name="arrow-back-circle-outline" size={37} color="black" />
+                </TouchableOpacity>}
 
             <Text style={{ fontSize: 25, fontFamily: 'appFont-semibold' }}>{title}</Text>
         </View>
